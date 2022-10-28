3c3329965f257a3536dbfc1f676cfb01f55adc39
YeeunJang <jangye0000@naver.com>
2022-10-28 11:00:20 +0900

    feat : js 추가

--- PORTFOLIO/main.js
+++ PORTFOLIO/main.js
@@ -1,1 +1,11 @@
 'use strict'; //꼭 써주도록
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log(`navbarHeight : ${navbarHeight}`);
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('navbar-dark');
+    }
+})
